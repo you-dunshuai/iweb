0621248a3851a385aec1d8982a5a6a5d86427814
you-dunshuai <70928617+you-dunshuai@users.noreply.github.com>
2020-09-25 09:09:27 +0800

    完成响应式轮播广告的组件完善

--- iweb_frontend/js/components.js
+++ iweb_frontend/js/components.js
@@ -13,7 +13,7 @@ document.querySelector('[data-close="nav"]').onclick=function(){
 let currentCarousel=0   //当前正在显示哪个广告
 let totalCarousel=4    //轮播广告的总数
 // 点击"下一张"按钮，自动切换到广告的下一张
-setInterval(document.querySelector('[data-carousel="next"]').onclick=function(){
+document.querySelector('[data-carousel="next"]').onclick=function(){
 	currentCarousel++
 	if(currentCarousel>=totalCarousel){
 		currentCarousel=0   //如果已经切换到最后一张之后，必须重回到0
@@ -30,7 +30,7 @@ setInterval(document.querySelector('[data-carousel="next"]').onclick=function(){
 	  list[currentCarousel].classList.add('active')
 	  
 	  
-},3000)
+}
 document.querySelector('[data-carousel="prev"]').onclick=function(){
 	currentCarousel--
 	if(currentCarousel<0){
@@ -48,3 +48,39 @@ document.querySelector('[data-carousel="prev"]').onclick=function(){
 	  list[currentCarousel].classList.add('active')
 	  
 }
+
+let indicatorList = document.querySelectorAll('[data-carousel-to]')
+// console.log(indicatorList)//类数组对象
+indicatorList.forEach(function(e,i){
+	//console.log(i,e)   //e就是每个li，i是li的下标
+	e.onclick=function(){
+		//切换要显示的广告项
+		document.querySelector('.carousel>.items>li.active').classList.remove('active')
+		//伪类选择器：nth-child(),选择第n个孩子，n从1开始，不是0
+		document.querySelector('.carousel>.items>li:nth-child(' + (i+1) + ')').classList.add('active')
+		//切换广告指示器的激活项
+		document.querySelector('.carousel>.indicators>li.active').classList.remove('active')
+		//伪类选择器：nth-child(),选择第n个孩子，n从1开始，不是0
+		document.querySelector('.carousel>.indicators>li:nth-child(' + (i+1) + ')').classList.add('active')
+	}
+})
+
+//一次性定时器
+// let timer=setTimeout()
+//周期性定时器
+let timer=setInterval(function(){
+	currentCarousel++
+	if(currentCarousel>=totalCarousel){
+		currentCarousel=0 //如果切换到最后一张后，则从头开始
+	}
+	//切换当前广告项
+	document.querySelector('.carousel>.items>li.active').classList.remove('active')
+	//伪类选择器：nth-child(),选择第n个孩子，n从1开始，不是0
+	document.querySelector('.carousel>.items>li:nth-child(' + (currentCarousel+1) + ')').classList.add('active')
+
+	//切换当前广告项对应的指示器
+	
+	document.querySelector('.carousel>.indicators>li.active').classList.remove('active')
+	//伪类选择器：nth-child(),选择第n个孩子，n从1开始，不是0
+	document.querySelector('.carousel>.indicators>li:nth-child(' + (currentCarousel+1) + ')').classList.add('active')
+},3000)
